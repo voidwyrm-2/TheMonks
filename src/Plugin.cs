9ceebfe46bca93cba52ef55c51bfa61d61a41e98
AutobotCoder <AutobotCoder@DESKTOP-4IO5JNK>
2023-05-25 14:01:19 -0400

    Still haven't reference DWP

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,12 +1,13 @@
 ﻿using BepInEx;
 using SlugBase.Features;
 using static SlugBase.Features.FeatureTypes;
-using Nutils;
+//using Nutils;
 using NuclearPasta.DualWieldingPlugin;
 
 namespace NuclearPasta.TheMonks
 {
     [BepInPlugin(MOD_ID, "The Monks", "1.0.0")]
+
     public class TheMonksMod : BaseUnityPlugin
     {
         private const string MOD_ID = "nuclearpasta.themonks";
@@ -19,7 +20,8 @@ namespace NuclearPasta.TheMonks
             On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
 
             // Put your custom hooks here!
-            On.Player.Grabability += new DualWieldingMod(DualWield).DoubleSpear;
+            //On.Player.Grabability += new DualWieldingMod(DualWield).DoubleSpear;
+            new DualWieldingMod(DualWield);
             //On.Player.Grabability += Player_DualWield;
         }
         
